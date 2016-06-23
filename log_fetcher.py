7975b9bd2262702f38178c13d0eef50b4463bf3e
lex <epam.lex@gmail.com>
2016-06-23 14:22:30 +0300

    syntax light changes

--- log_fetcher.py
+++ log_fetcher.py
@@ -12,7 +12,9 @@ except: OSError
 
 logging.basicConfig(filename=journal_path,level=logging.INFO)
 client = CloudifyClient('172.25.1.15')
-events = client.events.list(_include=["id,event_type","message","timestamp","context"], _sort="@timestamp", event_type=["workflow_started", "workflow_succeeded", "workflow_failed"])
+events = client.events.list(_include=["id,event_type","message","timestamp","context"], _sort="@timestamp",
+                            event_type=["workflow_started", "workflow_succeeded", "workflow_failed"])
+
 for event in events:
-    logging.info(str().join([event['timestamp'], " ", event['context']['blueprint_id'], " ", event['context']['deployment_id'], " ", event['message']["text"], " ", event['event_type']]))
-    #print event['timestamp'], event['context']['blueprint_id'], event['context']['deployment_id'], event['message']["text"], event['event_type']
+    logging.info(str().join([event['timestamp'], " ", event['context']['blueprint_id'], " ",
+                             event['context']['deployment_id'], " ", event['message']["text"], " ", event['event_type']]))
